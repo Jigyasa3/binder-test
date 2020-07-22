5ee25989c18b632f875a2cf4f23c57c831a0d5ed
Jigyasa Arora <arorajigyasa1992@gmail.com>
2020-07-22 11:26:39 +0900

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,5 @@
-#install.packages("https://cran.r-project.org/src/contrib/Archive/vcfR/vcfR_1.11.0.tar.gz", repos = NULL, type="source")
+install.packages("devtools")
+devtools::install_github(repo="knausb/vcfR")  #method1
+
+#install.packages("https://cran.r-project.org/src/contrib/Archive/vcfR/vcfR_1.11.0.tar.gz", repos = NULL, type="source") #method2
+
