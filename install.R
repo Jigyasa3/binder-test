3970100d4fcf4a24bb1835b96ab2bc146e543a90
Jigyasa Arora <arorajigyasa1992@gmail.com>
2020-07-22 11:06:15 +0900

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,1 @@
-install.packages("/Users/jigyasa.arora/Downloads/vcfR_1.11.0.tar", repos = NULL, type="source")
+#install.packages("https://cran.r-project.org/src/contrib/Archive/vcfR/vcfR_1.11.0.tar.gz", repos = NULL, type="source")
